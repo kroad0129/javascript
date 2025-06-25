cfa7249f629638cd1e59d92fe7227219a9362e18
kroad0129 <kroad0129@naver.com>
2025-06-25 21:35:18 +0900

    자바스크립트연습

---
+++ var,let,const/script.js
@@ -0,0 +1,20 @@
+/*
+var greeting = "hello";
+console.log(greeting);
+
+var greeting = "hi";
+console.log(greeting);
+
+var greeting = "how are you?";
+console.log(greeting);
+
+
+let greeting = "hello";
+console.log(greeting);
+
+greeting = "hi";
+console.log(greeting);
+*/
+
+const greeting = "hello";
+console.log(greeting);
