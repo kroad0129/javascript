f219e61e209172cc5027209f77227392637e5a51
kroad0129 <kroad0129@naver.com>
2025-07-04 21:55:17 +0900

    mac

---
+++ stopwatch/script.js
@@ -0,0 +1,9 @@
+const appendTens = document.getElementById('tens');
+const appendSecond = document.getElementById('seconds');
+const buttonStart = document.getElementById('button-start');
+const buttonStop = doucument.getElementById('button-stop');
+const buttonReset = doucument.getElementById('button-reset');
+
+let seconds = 0;
+let tens = 0;
+let interval = 0;
